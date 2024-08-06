e2ca25c9ae7da796b87fa1c4d9434976c3d340ea
bsramosl <steeven10f@gmail.com>
2024-08-05 22:19:00 -0500

    fech

--- app/producto/page.js
+++ app/producto/page.js
@@ -1,9 +1,41 @@
 
 
-export default function Producto(){
+async function loadProduct(){
+    const res = await fetch('https://fakestoreapi.com/products')
+    const data = await res.json()
+    return data;
+}
+
+
+async function Producto(){
+    const pro = await loadProduct();
     return(
         <div>
             <h1>Producto</h1>
+
+            <table>
+                <thead>
+
+                </thead>
+                <tbody>
+                {pro.map((product)=>(
+                <tr key={product.id}>
+                    <td>{product.id}</td>
+                    <td>{product.title}</td>
+                    <td>{product.price}</td>
+                    <td>{product.category}</td>
+                    <td>{product.rating.count}</td>
+                    <td>
+                        <a className="btn btn-info">Edit</a>
+                        <a className="btn btn-danger">Eliminar</a>
+                    </td>
+                </tr>
+            ))
+            }
+                </tbody>
+            </table>
         </div>
     );
 }
+
+export default Producto;
